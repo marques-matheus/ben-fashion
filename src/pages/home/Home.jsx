98ea055f720e44313755faca278bec7913e14e28
Matheus Marques <marques.m4th3u5@gmail.com>
2022-05-04 00:31:03 -0300

    inserção do texto na imagem

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -43,11 +43,43 @@ const Div = styled.a`
   color: black;
 `;
 
+const Luxury = styled.h2`
+  position: absolute;
+  left: 43px;
+  top: 230px;
+  font-style: italic;
+  font-weight: 700;
+  text-transform: uppercase;
+  color: #333333;
+  font-size: 3rem;
+  opacity: 0.6;
+  font-family: "Bodoni Moda", serif; ;
+`;
+
+const Fashion = styled(Luxury)`
+  left: 60.37px;
+  top: 269.64px;
+`;
+const Acessories = styled(Luxury)`
+  left: 65.24px;
+  top: 309.02px;
+`;
+
+const And = styled(Luxury)`
+  left: 36.71px;
+top: 317.24px;
+font-size: 2rem;
+`
+
 function Home() {
   return (
     <>
       <Cabecalho />
       <main>
+        <Luxury>Luxury</Luxury>
+        <Fashion>Fashion</Fashion>
+        <And>&</And>
+        <Acessories>Acessories</Acessories>
         <Img src={homeImg} alt="Home" />
         <Botao>EXPLORAR COLEÇÃO</Botao>
       </main>
